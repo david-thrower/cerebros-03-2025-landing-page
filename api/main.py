2fa6c069d5de74e195cbde9cdd3ad5a9e1e36717
David Thrower <david.thrower@outlook.com>
2025-03-07 00:18:29 -0500

    Update main.py
    Add uvicorn run to Pyhton code

--- api/main.py
+++ api/main.py
@@ -75,3 +75,6 @@ async def signup(
     db.refresh(user)
 
     return {"message": "Signup successful"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
